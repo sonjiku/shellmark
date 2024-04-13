9b3075c64c16876d7c991ea04735b003c8ae6aeb
Marc Schöchlin <schoechlin@osb-alliance.com>
2024-04-13 21:07:24 +0200

    Multiple improvements
    - Add bash completion
    - Fix "--go" flag by using a function
    - Fix directories with spaces in the name (quote in config)
    - Change sheebang to bash (should be available on all systems nowadays)
    - Allow the change of the directory without "-g" or "--go"
    - Improve documentation

---
+++ shellmark_bash_include.sh
@@ -0,0 +1,36 @@
+_shellmark()
+{
+    local cur=${COMP_WORDS[COMP_CWORD]}
+    local prev=${COMP_WORDS[COMP_CWORD-1]}
+    local next=""
+
+    local all_options="-d --delete -g --go -p --print -s --set -h --help -l --list -L --listdiri -r --replace"
+    local all_marks="$(shellmark -l|xargs)"
+
+    case $prev in
+      -d|--delete|-g|--go|-p|--print|-s|--set|-l|--list|-L|--listdir|-r|--replace)
+         next="$all_marks"
+      ;;
+      --help|-h)
+         next=""
+      ;;
+      *)
+         next="$all_options $all_marks"
+      ;;
+    esac
+
+    # Sort the options
+    COMPREPLY=( $(compgen -W "$next" -- "$cur") )
+}
+
+
+cdm(){
+   if ( [ "$1" = "-g" ] || [ "$1" = "--go" ] ) && [ -n "$2" ];then
+      cd "$(shellmark -p $2)"
+   elif ! [[ $1 == -* ]];then
+      cd "$(shellmark -p $1)"
+   else
+      shellmark $@
+   fi
+}
+complete -F _shellmark cdm
