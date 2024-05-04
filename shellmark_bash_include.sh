6596c014d1c13ba510e6c3be028160a5c23964e0
Marc Schöchlin <schoechlin@osb-alliance.com>
2024-05-04 10:23:49 +0200

    improve completion
    Signed-off-by: Marc Schöchlin <schoechlin@osb-alliance.com>

--- shellmark_bash_include.sh
+++ shellmark_bash_include.sh
@@ -7,18 +7,25 @@ _shellmark_bash_completion()
     local all_options="-d --delete -g --go -p --print -s --set -h --help -l --list -L --listdir -r --replace"
     local all_marks="$(shellmark -l|xargs)"
 
-    case $prev in
-      -d|--delete|-g|--go|-p|--print|-s|--set|-l|--list|-L|--listdir|-r|--replace)
-         next="$all_marks"
-      ;;
-      --help|-h)
-         next=""
-      ;;
-      *)
-         next="$all_marks $all_options"
-      ;;
-    esac
-
+    if [[ "$cur" =~ ^--? ]];then
+      next="$all_options"
+    else
+       case $prev in
+         -d|--delete|-g|--go|-p|--print|-s|--set|-l|--list|-L|--listdir|-r|--replace)
+            next="$all_marks"
+         ;;
+         --help|-h)
+            next=""
+         ;;
+         *)
+            if [ "$COMP_CWORD" == "1" ];then
+               next="$all_marks"
+            else
+               next="$(compgen -o dirnames -G "$(shellmark -p $prev)/*" --)"
+            fi
+         ;;
+       esac
+    fi
     # Sort the options
     COMPREPLY=( $(compgen -W "$next" -- "$cur") )
 }
@@ -28,7 +35,11 @@ cdm(){
    if ( [ "$1" = "-g" ] || [ "$1" = "--go" ] ) && [ -n "$2" ];then
       cd "$(shellmark -p $2)"
    elif ( [ -n "$1" ] && ! [[ $1 == -* ]] );then
-      cd "$(shellmark -p $1)"
+      if [ -n "$2" ];then
+         cd "$2"
+      else
+         cd "$(shellmark -p $1)"
+      fi
    else
       shellmark $@
    fi
